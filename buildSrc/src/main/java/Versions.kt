7c3f57737ec792ccaedb93d1fa2df0395ef43c0c
miraboh <myrabohuche@gmail.com>
2022-11-17 11:23:31 +0100

    first commit

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,85 @@
+object Versions {
+
+    const val retrofit = "2.9.0"
+    const val squareupRetrofit2Converter = "2.9.0"
+    const val okhttp3LoggingInterceptor = "5.0.0-alpha.4"
+
+    const val appcompat = "1.1.0"
+    const val activity = "1.2.0-rc01"
+    const val activityCompose = "1.3.0-alpha03"
+    const val appStartup = "1.1.0-beta01"
+    const val cardview = "1.0.0"
+    const val archTesting = "2.0.0"
+    const val arcore = "1.7.0"
+    const val benchmark = "1.0.0"
+    const val benchmarkMacro = "1.1.0-alpha02"
+    const val browser = "1.0.0"
+    const val compose = "1.1.1"
+    const val constraintLayout = "1.1.3"
+    const val core = "1.3.2"
+    const val coroutines = "1.6.0"
+    const val coroutinesTest = "1.6.0"
+    const val crashlytics = "17.2.2"
+    const val dataStore = "1.0.0-beta01"
+    const val drawerLayout = "1.1.0-rc01"
+    const val espresso = "3.1.1"
+    const val firebaseAnalytics = "17.4.0"
+    const val firebaseAuth = "19.3.1"
+    const val firebaseConfig = "19.2.0"
+    const val firebaseFirestore = "21.4.3"
+    const val firebaseFunctions = "19.0.2"
+    const val firebaseMessaging = "20.1.6"
+    const val firebaseUi = "7.2.0"
+    const val flexbox = "3.0.0"
+    const val fragment = "1.3.0"
+    const val glide = "4.13.2"
+    const val googlePlayServicesMapsKtx = "3.0.0"
+    const val googlePlayServicesVision = "17.0.2"
+    const val gson = "2.8.6"
+    const val hamcrest = "1.3"
+    const val hilt = "2.42"
+    const val junit = "4.13"
+    const val junitExt = "1.1.1"
+    const val lifecycle = "2.4.0-alpha01"
+    const val lottie = "3.4.0"
+    const val material = "1.4.0-beta01"
+    const val mockito = "3.3.1"
+    const val mockitoKotlin = "1.5.0"
+    const val okhttp = "3.10.0"
+    const val okio = "1.14.0"
+    const val pageIndicator = "1.3.0"
+    const val playCore = "1.6.5"
+    const val room = "2.4.2"
+    const val rules = "1.1.1"
+    const val runner = "1.2.0"
+    const val slidingpanelayout = "1.2.0-alpha01"
+    const val threetenabp = "1.0.5"
+    const val timber = "4.7.1"
+    const val viewpager2 = "1.0.0"
+    const val viewModelCompose = "1.0.0-alpha02"
+    const val uiAutomator = "2.2.0"
+
+
+    /**
+     * Build version
+     */
+
+    val versionName = "1.0.0" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+    val versionCodeBase = 0 // XYYZZM; M = Module (mobile)
+    val versionCodeMobile = versionCodeBase + 1
+
+    const val COMPILE_SDK = 32
+    const val TARGET_SDK = 30
+    const val MIN_SDK = 21
+
+    const val ANDROID_GRADLE_PLUGIN = "7.1.2"
+    const val BENCHMARK = "1.0.0"
+    const val COMPOSE = "1.1.1"
+    const val FIREBASE_CRASHLYTICS = "2.3.0"
+    const val GOOGLE_SERVICES = "4.3.3"
+    const val KOTLIN = "1.6.10"
+    const val NAVIGATION = "2.5.1"
+    const val HILT_AGP = "2.42"
+
+    const val THREETENBP = "1.3.6"
+}
---
+++ model/src/main/java/com/zovack/model/MyClass.kt
@@ -0,0 +1,4 @@
+package com.zovack.model
+
+class MyClass {
+}
